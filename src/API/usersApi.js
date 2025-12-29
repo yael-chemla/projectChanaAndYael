dff77764b21c76b72063b11e42e090f1dea772ce
yael chemlagit config -- user.email 6503yael@gmail.com <6503yael@gmail.com>
2025-12-29 23:39:54 +0200

    השלמה

--- src/API/usersApi.js
+++ src/API/usersApi.js
@@ -1,40 +1,8 @@
-// // src/api.js
-
-// // GET - מביא את כל המשתמשים
-// export const getUsers = async () => {
-//   try {
-//     const response = await fetch('http://localhost:3001/users');
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error('Error fetching users:', error);
-//     return [];
-//   }
-// };
-
-// // POST - יוצר משתמש חדש
-// export const createUser = async (user) => {
-//   try {
-//     const response = await fetch('http://localhost:3001/users', {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(user)
-//     });
-
-//     if (!response.ok) throw new Error("שגיאה ביצירת המשתמש");
-
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error(error);
-//     throw error;
-//   }
-// };
 
 // GET - מביא את כל המשתמשים
 export const getUsers = async () => {
   try {
-    const response = await fetch('http://localhost:3001/users');
+    const response = await fetch('http://localhost:3000/users');
     const data = await response.json();
     return data;
   } catch (error) {
@@ -46,7 +14,7 @@ export const getUsers = async () => {
 // POST - יוצר משתמש חדש
 export const createUser = async (user) => {
   try {
-    const response = await fetch('http://localhost:3001/users', {
+    const response = await fetch('http://localhost:3000/users', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user)
