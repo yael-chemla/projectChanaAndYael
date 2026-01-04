7024a42437883e97aad4e761c36b5add07019d33
yael chemlagit config -- user.email 6503yael@gmail.com <6503yael@gmail.com>
2026-01-04 19:13:44 +0200

    19-13 04-01-2026

--- src/components/myPosts/Comment.jsx
+++ src/components/myPosts/Comment.jsx
@@ -1,10 +1,9 @@
 import { useState, useContext } from "react";
 import { MyContext } from "../../context";
 
-function Comment({ comment, onUpdate, onDelete }) {
+function Comment({ comment, onUpdate, onDelete,isEditing,setIsEditing }) {
     const { currentUser } = useContext(MyContext);
     const [newBody, setNewBody] = useState(comment.body);
-    const [isEditing, setIsEditing] = useState(false);
 
     const handleSave = () => {
         onUpdate(comment.id, { body: newBody });
